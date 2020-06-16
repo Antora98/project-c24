414a07868693807d87247976a6e4b3d29b0cdaaf
vs-coding-class <65456951+vs-coding-class@users.noreply.github.com>
2020-06-16 09:22:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj, paperObject,groundObject	
+
+var dustbinObj, paperObject,groundObject;
 var world;
 
 
@@ -12,28 +12,14 @@ function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
 
-
 	engine = Engine.create();
 	world = engine.world;
-	dustbinObj=new dustbin(1200,650);
-	paperObject=new paper(200,450,40);
-	groundObject=new ground(width/2,670,width,20);
-	//Create a Ground
-	
 
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paperBall();
+	groundObject=new Ground();
 
 	Engine.run(engine);
-	//Render.run(render);
-  
 }
 
 
@@ -44,22 +30,10 @@ function draw() {
   dustbinObj.display();
   paperObject.display();
   groundObject.display();
-  
- 
-  
-  
- 
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
-
     	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-    
   	}
 }
-
-
-
-
-
